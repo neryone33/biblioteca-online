da50f5a762831ccc058af72eabb5eaa31fca0a1e
Jucinery <franciscojucinery@gmail.com>
2025-06-08 13:50:12 -0300

    feat: Adiciona configuração para deploy em produção

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+// Objeto base da configuração
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  logging: false,
+};
+
+// Adiciona opções SSL apenas se estiver em ambiente de produção
+if (process.env.NODE_ENV === 'production') {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: true,
     }
-  }
-);
+  };
+}
+
+const sequelize = new Sequelize(config);
 
 module.exports = sequelize;
