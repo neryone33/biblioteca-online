de4d5372e24e7b120708aefc630eb098b1fd6e81
Jucinery <franciscojucinery@gmail.com>
2025-06-08 14:36:44 -0300

    atualizando script de procedures-triggers

--- scripts/procedures-triggers.sql
+++ scripts/procedures-triggers.sql
@@ -1,6 +1,6 @@
-USE BibliotecaOnline;
+-- Define o delimitador para // para que a procedure possa ser lida como um bloco único
+DELIMITER //
 
--- Procedure para realizar empréstimo
 CREATE PROCEDURE RealizarEmprestimo(
     IN p_usuario_id INT,
     IN p_livro_id INT,
@@ -8,26 +8,26 @@ CREATE PROCEDURE RealizarEmprestimo(
 )
 BEGIN
     DECLARE v_disponivel INT;
-
+    
     -- Verificar disponibilidade
     SELECT quantidade_disponivel INTO v_disponivel 
     FROM Livros WHERE id = p_livro_id;
-
+    
     IF v_disponivel > 0 THEN
         -- Inserir empréstimo
         INSERT INTO Emprestimos (usuario_id, livro_id, data_devolucao_prevista)
         VALUES (p_usuario_id, p_livro_id, p_data_devolucao_prevista);
-
+        
         -- Atualizar disponibilidade
         UPDATE Livros 
         SET quantidade_disponivel = quantidade_disponivel - 1
         WHERE id = p_livro_id;
-
+        
         SELECT 'Empréstimo realizado com sucesso' as mensagem;
     ELSE
         SELECT 'Livro não disponível' as mensagem;
     END IF;
-END;
+END //
 
 -- Trigger para atualizar disponibilidade na devolução
 CREATE TRIGGER AtualizarDisponibilidadeAposDevolucao
@@ -39,7 +39,7 @@ BEGIN
         SET quantidade_disponivel = quantidade_disponivel + 1
         WHERE id = NEW.livro_id;
     END IF;
-END;
+END //
 
 -- Trigger para impedir empréstimo de livro indisponível
 CREATE TRIGGER VerificarDisponibilidadeAntesEmprestimo
@@ -47,12 +47,15 @@ CREATE TRIGGER VerificarDisponibilidadeAntesEmprestimo
     FOR EACH ROW
 BEGIN
     DECLARE v_disponivel INT;
-
+    
     SELECT quantidade_disponivel INTO v_disponivel 
     FROM Livros WHERE id = NEW.livro_id;
-
+    
     IF v_disponivel <= 0 THEN
         SIGNAL SQLSTATE '45000' 
         SET MESSAGE_TEXT = 'Livro não disponível para empréstimo';
     END IF;
-END;
+END //
+
+-- Retorna o delimitador para o padrão (ponto e vírgula)
+DELIMITER ;
